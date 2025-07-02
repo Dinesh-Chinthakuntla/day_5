7b04e7508d26418b54960fc8a6058c50fa13987e
Dinesh Chinthakuntla <dineshchinthakuntla@gmail.com>
2025-07-02 14:37:54 +0530

    updates

---
+++ range.py
@@ -0,0 +1,9 @@
+a=range(1)     #single parameter
+b=range(1,10)  #double parameters
+c=range(2,55,3)# triple parameters
+print(type(a))
+print(a)
+print(type(b))
+print(b)
+print(type(c))
+print(c)
---
+++ set.py
@@ -0,0 +1,7 @@
+a={1,2,3,4,5}
+b={1,2.0,3j,"dinesh"}
+print(a)
+print(type(a))
+
+print(type(b))
+print(b)
